c9a75c91097bc4b3ea527d6c1ba78a96056ad053
theadland <16597457+theadland@users.noreply.github.com>
2021-10-27 13:42:48 -0600

    Program is now mostly a copy of the windows D2D1 simple demo example, having some linking problems.

---
+++ CS365 Project/MainWindow.h
@@ -0,0 +1,142 @@
+#pragma once
+
+// Modify the following defines if you have to target a platform prior to the ones specified below.
+// Refer to MSDN for the latest info on corresponding values for different platforms.
+#ifndef WINVER              // Allow use of features specific to Windows 7 or later.
+#define WINVER 0x0A00       // Change this to the appropriate value to target other versions of Windows.
+#endif
+
+#ifndef _WIN32_WINNT        // Allow use of features specific to Windows 7 or later.
+#define _WIN32_WINNT 0x0A00 // Change this to the appropriate value to target other versions of Windows.
+#endif
+
+#ifndef UNICODE
+#define UNICODE
+#endif 
+
+#define WIN32_LEAN_AND_MEAN     // Exclude rarely-used stuff from Windows headers
+#include <Windows.h>
+#include <stdexcept>
+
+// C RunTime Header Files
+#include <stdlib.h>
+#include <malloc.h>
+#include <memory.h>
+#include <wchar.h>
+#include <math.h>
+
+#include <d2d1_1.h>
+#include <d2d1_1helper.h>
+#include <dwrite.h>
+#include <wincodec.h>
+#pragma comment(lib, "d2d1")
+
+/******************************************************************
+*                                                                 *
+*  Macros                                                         *
+*                                                                 *
+******************************************************************/
+
+template<class Interface>
+inline void
+SafeRelease(
+	Interface** ppInterfaceToRelease
+)
+{
+	if (*ppInterfaceToRelease != NULL)
+	{
+		(*ppInterfaceToRelease)->Release();
+
+		(*ppInterfaceToRelease) = NULL;
+	}
+}
+
+#ifndef Assert
+#if defined( DEBUG ) || defined( _DEBUG )
+#define Assert(b) do {if (!(b)) {OutputDebugStringA("Assert: " #b "\n");}} while(0)
+#else
+#define Assert(b)
+#endif //DEBUG || _DEBUG
+#endif
+
+
+#ifndef HINST_THISCOMPONENT
+EXTERN_C IMAGE_DOS_HEADER __ImageBase;
+#define HINST_THISCOMPONENT ((HINSTANCE)&__ImageBase)
+#endif
+
+/******************************************************************
+*                                                                 *
+*  MainWindow                                                     *
+*                                                                 *
+******************************************************************/
+
+class MainWindow
+{
+public:
+	MainWindow();
+	~MainWindow();
+
+	// Create application window and initialize device-indedendent resources
+	HRESULT Initialize();
+
+	// Create resources not bound to any device
+	HRESULT CreateDeviceIndependentResources();
+
+	// Create resourcs bound to a particular device
+	HRESULT CreateDeviceResources();
+
+	HRESULT CreateGridPatternBrush(
+		ID2D1RenderTarget* pRenderTarget,
+		ID2D1BitmapBrush** ppBitmapBrush
+	);
+
+	void DiscardDeviceResources();
+
+	HRESULT OnRender();
+
+	void OnResize(
+		UINT width,
+		UINT height
+	);
+
+	HRESULT LoadResourceBitmap(
+		ID2D1RenderTarget* pRenderTarget,
+		IWICImagingFactory* pIWICFactory,
+		PCWSTR resourceName,
+		PCWSTR resourceType,
+		UINT destinationWidth,
+		UINT destinationHeight,
+		ID2D1Bitmap** ppBitmap
+	);
+
+	HRESULT LoadBitmapFromFile(
+		ID2D1RenderTarget* pRenderTarget,
+		IWICImagingFactory* pIWICFactory,
+		PCWSTR uri,
+		UINT destinationWidth,
+		UINT destinationHeight,
+		ID2D1Bitmap** ppBitmap
+	);
+
+	// Main window message loop
+	void RunMessageLoop();
+
+	// the WindowProc function prototype
+	static LRESULT CALLBACK WindowProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam);
+
+private:
+	HWND m_hwnd;
+	ID2D1Factory* m_pD2DFactory;
+	IWICImagingFactory* m_pWICFactory;
+	IDWriteFactory* m_pDWriteFactory;
+	ID2D1HwndRenderTarget* m_pRenderTarget;
+	IDWriteTextFormat* m_pTextFormat;
+	ID2D1PathGeometry* m_pPathGeometry;
+	ID2D1LinearGradientBrush* m_pLinearGradientBrush;
+	ID2D1SolidColorBrush* m_pBlackBrush;
+	ID2D1BitmapBrush* m_pGridPatternBitmapBrush;
+	ID2D1Bitmap* m_pBitmap;
+	ID2D1Bitmap* m_pAnotherBitmap;
+};
+
--- CS365 Project/main.cpp
+++ CS365 Project/main.cpp
@@ -1,22 +1,6 @@
-#ifndef UNICODE
-#define UNICODE
-#endif 
 
-#include <windows.h>
-#include <stdexcept>
 
-const wchar_t app_title[] = L"CS365 Project";
-
-struct StateInfo {
-	// not yet in use
-	// https://docs.microsoft.com/en-us/windows/win32/learnwin32/managing-application-state-
-};
-
-// the WindowProc function prototype
-LRESULT CALLBACK WindowProc(HWND hwnd,
-	UINT message,
-	WPARAM wParam,
-	LPARAM lParam);
+#include "MainWindow.h"
 
 // the entry point for any Windows program
 int WINAPI wWinMain(HINSTANCE hInstance,
@@ -24,103 +8,19 @@ int WINAPI wWinMain(HINSTANCE hInstance,
 	PWSTR pCmdLine,
 	int nCmdShow)
 {
-	// Register the window class
-	const wchar_t CLASS_NAME[] = L"Sample Window Class";
-
-	// State initialization info to pass to main window
-	StateInfo* pState = new (std::nothrow) StateInfo;
-
-	// this struct holds information for the window class
-	WNDCLASS wc = { };
-
-	// fill in the struct with the needed information
-	wc.lpfnWndProc = WindowProc;				// pointer to WindowProc callback
-	wc.hInstance = hInstance;					// application instance handle
-	wc.hCursor = LoadCursor(NULL, IDC_ARROW);
-	wc.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
-	wc.lpszClassName = CLASS_NAME;				// window class name
-
-	// register the window class with the operating system
-	RegisterClass(&wc);
-
-	// adjust window size based on client size
-	RECT wr = { 0, 0, 500, 400 };
-	AdjustWindowRect(&wr, WS_OVERLAPPEDWINDOW, false);
-
-	// create the window and use the result as the handle
-	HWND hwnd = CreateWindowEx(0,
-		CLASS_NAME,				// name of the window class
-		app_title,				// title of the window
-		WS_OVERLAPPEDWINDOW,	// window style
-		300,					// x-position of window
-		300,					// y-position of window
-		wr.right - wr.left,		// width of the window
-		wr.bottom - wr.top,		// height of the window
-		NULL,					// we have no parent window, so null
-		NULL,					// we aren't using menus, so null
-		hInstance,				// application handle
-		pState);				// pointer to struct passing state infomation
-
-	if (hwnd == NULL)
+	
+	if (SUCCEEDED(CoInitialize(NULL))) // Initialize Windows COM library
 	{
-		return 0;
-	}
-
-	//display window on the screen
-	ShowWindow(hwnd, nCmdShow);
-
-	// -- MAIN LOOP --
-
-	// this struct holds Windows event messages
-	MSG msg;
-	// wait for the next message in the queue, store the result in 'msg'
-	while (GetMessage(&msg, NULL, 0, 0))
-	{
-		TranslateMessage(&msg); // translate keystroke messages into the right format
-		DispatchMessage(&msg);  // send the message tot hte WindowProc function
-	}
-
-	// return this part of the WM_QUIT message to Windows
-	return msg.wParam;
-}
-
-// this is the main message handler for the program and where application
-// behaviour is defined
-LRESULT CALLBACK WindowProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
-{
-	// sort through and find what code to run for the message given
-	switch (message)
-	{
-		//this message is read when the window is clased
-	case WM_DESTROY:
-	{
-		// close the application entirely
-		PostQuitMessage(0); // message causes GetMessage to return 0, ends while loop above
-		return 0;			 // tells windows message was handled
-	}
-	case WM_CLOSE:
-	{
-		if (MessageBox(hwnd, L"Are you sure you want to Exit?", app_title, MB_OKCANCEL) == IDOK)
 		{
-			DestroyWindow(hwnd);
+			MainWindow mainWindow;
+
+			if (SUCCEEDED(mainWindow.Initialize()))
+			{
+				mainWindow.RunMessageLoop();
+			}
 		}
-		// Else: User canceled. Do nothing.
-		return 0;
+		CoUninitialize(); // Unitialize Windows COM library
 	}
-	case WM_PAINT:
-	{
-		PAINTSTRUCT ps;
-		HDC hdc = BeginPaint(hwnd, &ps);
-
-		// All painting occurs here, between BeginPaint and EndPaint.
-
-		FillRect(hdc, &ps.rcPaint, (HBRUSH)(COLOR_WINDOW + 1));
-
-		EndPaint(hwnd, &ps);
-	}
+	
 	return 0;
-
-	}
-	// Handle any messages the switch statement didn't
-	return DefWindowProc(hwnd, message, wParam, lParam);
 }
